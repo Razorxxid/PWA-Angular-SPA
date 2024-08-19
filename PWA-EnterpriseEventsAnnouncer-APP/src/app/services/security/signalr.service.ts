64b03e867ff0a188768966aa91367d1bbec11875
Tomas Colazo <tomascolaz@gmail.com>
2024-08-18 22:20:41 -0300

    changed hub service url

--- PWA-EnterpriseEventsAnnouncer-APP/src/app/services/security/signalr.service.ts
+++ PWA-EnterpriseEventsAnnouncer-APP/src/app/services/security/signalr.service.ts
@@ -27,10 +27,10 @@ export class SignalRService {
     if (tokenCheck) {
       token = tokenCheck;
     }
-
     this.hubConnection = new signalR.HubConnectionBuilder()
-    .withUrl("https://pwa-gremio-api.azure-api.net/pwa-gremio-api/GremioHubService", { 
+    .withUrl("https://pwa-gremio-api.azure-api.net/pwa-gremio-api/api/GremioHubService/", { 
       accessTokenFactory: () => token,
+      withCredentials: false
       
     })
     .configureLogging(signalR.LogLevel.Debug)
@@ -152,3 +152,4 @@ export class SignalRService {
 
   
 }
+
